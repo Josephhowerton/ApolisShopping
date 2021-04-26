baee9d5e4d36dfb340c57d6070a62959996eed6d
master baby yoda <josephkhowerton@gmail.com>
2021-04-25 19:22:54 -0700

    initial commit

--- app/src/main/java/com/josephhowerton/apolisshopping/app/Config.kt
+++ app/src/main/java/com/josephhowerton/apolisshopping/app/Config.kt
@@ -3,35 +3,29 @@ package com.josephhowerton.apolisshopping.app
 class Config {
     companion object{
         val BANNER_IMAGE = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUo8ynBItVwCdYRCNj9cibTm2nud8gACmfsA&usqp=CAU"
-        val BASE_URL = "http://grocery-second-app.herokuapp.com/api/"
-        val BASE_IMAGE_URL = "http://rjtmobile.com/grocery/images/"
 
         fun getBaseUrlWithEndpoint(endpoint:String): String{
-            return "${"$BASE_URL$endpoint/$id"}?"
+            return "http://grocery-second-app.herokuapp.com/api/${endpoint}?"
         }
 
         fun getBaseUrlWithEndpoint(endpoint:String, id:Int): String{
-            return "${"$BASE_URL$endpoint/$id"}?"
+            return "http://grocery-second-app.herokuapp.com/api/${endpoint}/${id}?"
         }
 
         fun getBaseUrlWithProductEndpoint(endpoint:String, id:Int): String{
-            return "${"$BASE_URL$endpoint/sub/$id"}?"
+            return "http://grocery-second-app.herokuapp.com/api/${endpoint}/sub/${id}?"
         }
 
         fun getBaseUrlWithProductDetailsEndpoint(endpoint:String, id:String): String{
-            return "${"$BASE_URL$endpoint/$id"}?"
+            return "http://grocery-second-app.herokuapp.com/api/${endpoint}/${id}?"
         }
 
         fun getImageUrlWithCategoryId(categoryId:Int): String{
-            return "${"$BASE_IMAGE_URL$categoryId"}?"
+            return "http://rjtmobile.com/grocery/images/${categoryId}?"
         }
 
         fun getImageUrlWithCategoryId(categoryId:String): String{
-            return "${"$BASE_IMAGE_URL$categoryId"}?"
-        }
-
-        fun getRegister(): String{
-            return "$BASE_URL + ${Endpoint.URL_REGISTER}"
+            return "http://rjtmobile.com/grocery/images/${categoryId}?"
         }
 
         val TITLE = "There was an error!"
